cbe5760246fb89bd952a63937506bfe0f16f9d93
S. KOYAMA <ko.yama.s0916@gmail.com>
2022-01-20 20:27:11 +0900

    show the results of wilcox tests by asterisc

--- S76BKO_colony.R
+++ S76BKO_colony.R
@@ -4,7 +4,7 @@ d <- read.delim("colony diamiter.txt", header = TRUE)
 d_S76BKO <-  d [1:36,1:2]
 library(ggplot2)
 theme_set(theme_set(theme_classic() + theme(legend.position = "top")))
-p_S76BKO <- ggplot(d_S76BKO, aes(x=strain, y=diameter)) + geom_boxplot()
+p_S76BKO <- ggplot(d_S76BKO, aes(x=strain, y=diameter)) + geom_boxplot(width = 0.4)
 p_S76BKO
 p_S76BKO <- p_S76BKO + scale_x_discrete(limit=c('WT', 'E.IS76', '76BKO-6', '76BKO-8'))
 p_S76BKO
@@ -16,3 +16,28 @@ p_S76BKO <- p_S76BKO + labs(
     axis.title = element_text(size = 14)
     )
 p_S76BKO
+library("ggsignif")
+p2_S76BKO <- p_S76BKO + geom_signif(comparisons = list(c("WT", "E.IS76")),
+                                    test = "wilcox.test",
+                                    na.rm = FALSE,
+                                    map_signif_level = c("****" = 0.0001, "***" = 0.001, "**" = 0.01, "*" = 0.05),
+                                    y_position = 4.7)
+p2_S76BKO
+p3_S76BKO <- p2_S76BKO + geom_signif(comparisons = list(c("WT", "76BKO-6")),
+                                    test = "wilcox.test",
+                                    na.rm = FALSE,
+                                    map_signif_level = c("****" = 0.0001, "***" = 0.001, "**" = 0.01, "*" = 0.05),
+                                    y_position = 5.0)
+p3_S76BKO
+p4_S76BKO <- p3_S76BKO + geom_signif(comparisons = list(c("WT", "76BKO-8")),
+                                     test = "wilcox.test",
+                                     na.rm = FALSE,
+                                     map_signif_level = c("****" = 0.0001, "***" = 0.001, "**" = 0.01, "*" = 0.05),
+                                     y_position = 5.3)
+p4_S76BKO
+p5_S76BKO <- p4_S76BKO + geom_signif(comparisons = list(c("E.IS76", "76BKO-6"), c("E.IS76", "76BKO-8")),
+                                     test = "wilcox.test",
+                                     na.rm = FALSE,
+                                     map_signif_level = c("****" = 0.0001, "***" = 0.001, "**" = 0.01, "*" = 0.05),
+                                     y_position = 3.2)
+p5_S76BKO
