636def8821e8e9ec5878aac37f4d5b889f7efab5
S. KOYAMA <ko.yama.s0916@gmail.com>
2022-01-20 19:39:10 +0900

    change the order of x_axis

--- S76BKO_colony.R
+++ S76BKO_colony.R
@@ -4,6 +4,7 @@ d <- read.delim("colony diamiter.txt", header = TRUE)
 d_S76BKO <-  d [1:36,1:2]
 library(ggplot2)
 theme_set(theme_set(theme_classic() + theme(legend.position = "top")))
-p_S76BKO <- ggplot(d_S76BKO, aes(x=strain, y=diameter))
-p_S76BKO + geom_boxplot()
-
+p_S76BKO <- ggplot(d_S76BKO, aes(x=strain, y=diameter)) + geom_boxplot()
+p_S76BKO
+p_S76BKO <- p_S76BKO + scale_x_discrete(limit=c('WT', 'E.IS76', '76BKO-6', '76BKO-8'))
+p_S76BKO
