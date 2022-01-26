612111efde78035c046fb4452a9c16626b42d07e
S-Koyama9 <ko.yama.s0916@gmail.com>
2022-01-26 16:25:20 +0900

    make new plots

---
+++ frqKO_cellfusion.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+d_frq_fusion <- read.csv("frq_cellfusion.csv")
+library(scales)
+library(ggplot2)
+theme_set(theme_set(theme_classic() + theme(legend.position = "top")))
+yRoof_frq_fus <- round(max(d_frq_fusion[,2])*1.2,1) 
+p_frq_fusion <- ggplot(d_frq_fusion, aes(x=strain, y=fusion)) + geom_boxplot(width = 0.4)
+p_frq_fusion <- p_frq_fusion + scale_x_discrete(limit=c('WT', 'Δfrq_5', 'Δfrq_8'))
+p_frq_fusion <- p_frq_fusion + ylim(0,yRoof_frq_fus)
+p_frq_fusion <- p_frq_fusion + theme_classic()
+p_frq_fusion <- p_frq_fusion + labs(
+  y = expression(paste ("Number of cell-fusion expression ( ", {  mm^2 }, ")", sep="  ") ), x= ""
+)+
+  theme(
+    axis.title = element_text(size = 12)
+  )
+p_frq_fusion
