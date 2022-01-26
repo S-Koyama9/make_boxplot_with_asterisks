6a463cb672a9f1509cdbc0c1c5ea7d1ee8e9747e
S-Koyama9 <ko.yama.s0916@gmail.com>
2022-01-26 16:35:29 +0900

    add statistic changes

--- frqKO_cellfusion.R
+++ frqKO_cellfusion.R
@@ -8,7 +8,14 @@ p_frq_fusion <- ggplot(d_frq_fusion, aes(x=strain, y=fusion)) + geom_boxplot(wid
   scale_x_discrete(limit=c('WT', 'Δfrq_5', 'Δfrq_8'))+
   ylim(0,yRoof_frq_fus)+
   theme_classic()+ 
-  labs(y = expression(paste ("Number of cell-fusion expression ( ", {  mm^2 }, ")", sep="  ") ), x= "")+
+  labs(y = expression(paste ("Number of cell-fusion ( / ", {  mm^2 }, ")", sep="  ") ), x= "")+
   theme(axis.title = element_text(size = 12)
   )
 p_frq_fusion
+library("ggsignif")
+p2_frq_fusion <- p_frq_fusion + geom_signif(comparisons = list(c("WT", "Δfrq_5"), c("WT", "Δfrq_8")),
+                                            test = "t.test",
+                                            na.rm = FALSE,
+                                            map_signif_level = c("****" = 0.0001, "***" = 0.001, "**" = 0.01, "*" = 0.05),
+                                            y_position = yRoof_frq_fus*0.9)
+p2_frq_fusion
