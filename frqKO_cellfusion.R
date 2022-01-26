cc7bad8ba64c8db14c9d50d3677e3ce40a5f0f4f
S-Koyama9 <ko.yama.s0916@gmail.com>
2022-01-26 16:30:08 +0900

    delete sume lines
    delete duplicated lines by +

--- frqKO_cellfusion.R
+++ frqKO_cellfusion.R
@@ -4,14 +4,11 @@ library(scales)
 library(ggplot2)
 theme_set(theme_set(theme_classic() + theme(legend.position = "top")))
 yRoof_frq_fus <- round(max(d_frq_fusion[,2])*1.2,1) 
-p_frq_fusion <- ggplot(d_frq_fusion, aes(x=strain, y=fusion)) + geom_boxplot(width = 0.4)
-p_frq_fusion <- p_frq_fusion + scale_x_discrete(limit=c('WT', 'Δfrq_5', 'Δfrq_8'))
-p_frq_fusion <- p_frq_fusion + ylim(0,yRoof_frq_fus)
-p_frq_fusion <- p_frq_fusion + theme_classic()
-p_frq_fusion <- p_frq_fusion + labs(
-  y = expression(paste ("Number of cell-fusion expression ( ", {  mm^2 }, ")", sep="  ") ), x= ""
-)+
-  theme(
-    axis.title = element_text(size = 12)
+p_frq_fusion <- ggplot(d_frq_fusion, aes(x=strain, y=fusion)) + geom_boxplot(width = 0.4) +
+  scale_x_discrete(limit=c('WT', 'Δfrq_5', 'Δfrq_8'))+
+  ylim(0,yRoof_frq_fus)+
+  theme_classic()+ 
+  labs(y = expression(paste ("Number of cell-fusion expression ( ", {  mm^2 }, ")", sep="  ") ), x= "")+
+  theme(axis.title = element_text(size = 12)
   )
 p_frq_fusion
